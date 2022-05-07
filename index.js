08e0e2d3101fe389e19b37b27067842707c20d71
Priscilla Celes Lima <pri.c.maciel@hotmail.com>
2022-05-07 17:23:38 -0300

    calculator-operations
    Implementação das operação da calculadora e do histórico, no HTML, CSS e JS.

--- index.js
+++ index.js
@@ -1,86 +1,141 @@
-const $buttonsNumber = document.querySelectorAll('.calculator__buttons--number')
-const $buttonsMathOperations = document.querySelectorAll('.calculator__buttons--operation')
+//* CONSTANTS
+const $buttonsNumber = document.querySelectorAll(".calculator__buttons--number");
+const $buttonsMathOperations = document.querySelectorAll(".calculator__buttons--operation");
 
-const $buttonmultiply = document.querySelector('.calculator__buttons--operation-mult')
-const $buttonDivision = document.querySelector('.calculator__buttons--operation-divi')
-const $buttonParenthesis = document.querySelector('.calculator__buttons--operation-parenthesis')
-const $buttonPercentage = document.querySelector('.calculator__buttons--operation-percentage')
+const $buttonCallback = document.querySelector(".calculator__display-callback");
+const $memory = document.querySelector(".calculator__memory");
+const $textMemory = document.querySelector(".calculator__memory--text-calc");
+const $resultMemory = document.querySelector(".calculator__memory--text-result");
+const $closeCallback = document.querySelector(".calculator__memory--close");
 
-const $displayInput = document.querySelector('.calculator__display-input')
+const $buttonParenthesis = document.querySelector(".calculator__buttons--operation-parenthesis");
+const $buttonPercentage = document.querySelector(".calculator__buttons--operation-percentage");
+const $buttonSignal = document.querySelector(".calculator__buttons--signal");
 
-const $result = document.querySelector('.calculator__display-result')
+const $displayInput = document.querySelector(".calculator__display-input");
 
-const $resultButton = document.querySelector('.calculator__buttons--result')
-const $resetButton = document.querySelector('.calculator__buttons--reset')
+const $result = document.querySelector(".calculator__display-result");
 
-let calculateConvertToNumber = 0 
-let multiply = false
-let division = false
-let parenthesis = false
-let result = 0
+const $resultButton = document.querySelector(".calculator__buttons--result");
+const $resetButton = document.querySelector(".calculator__buttons--reset");
 
+//* VARIABLES
+let calculateConvertToNumber = 0;
+let parenthesis = false;
+let result = 0;
+let operation = false;
+let callbackMemory = []
+let calc = ''
+let finalResult = ''
 
-$buttonsNumber.forEach(function($button){
-    $button.addEventListener('click', function(){
-        $displayInput.value = $displayInput.value + $button.textContent
-        calculateConvertToNumber += Number($displayInput.value)
-        console.log(calculateConvertToNumber)
-    })
-})
+//* FUNCTIONS
+function printMemory() {
+  if(calc != '' && finalResult != ''){
+    $textMemory.innerHTML = calc + '='
+    $resultMemory.innerHTML = finalResult
+  }
+}
 
-$buttonsMathOperations.forEach(function($button){
-    $button.addEventListener('click', function() {
-        if($result.textContent == 0){
-        $displayInput.value = $displayInput.value + $button.textContent
-        console.log(calculateConvertToNumber)
-        } else {
-            $displayInput.value = $result.textContent + $button.textContent
+function getResult() {
+  if ($displayInput.value.includes("×")) {
+    result = $displayInput.value.replaceAll("×", " ");
+    result = result.split(" ");
+    $result.textContent = result[0] * result[1];
+  }
 
-        }
-    })
-})
+  if ($displayInput.value.includes("÷")) {
+    result = $displayInput.value.replaceAll("÷", " ");
+    result = result.split(" ");
+    $result.textContent = result[0] / result[1];
+  }
 
-$buttonmultiply.addEventListener('click', function() {
-    multiply = true
-})
+  if ($displayInput.value.includes("+")) {
+    result = $displayInput.value.replaceAll("+", " ");
+    result = result.split(" ");
+    $result.textContent = Number(result[0]) + Number(result[1]);
+  }
 
-$buttonDivision.addEventListener('click', function() {
-    division = true
-})
+  if ($displayInput.value.includes("-")) {
+    result = $displayInput.value.replaceAll("-", " ");
+    result = result.split(" ");
+    $result.textContent = Number(result[0]) - Number(result[1]);
+  }
+  operation = false
 
-$buttonParenthesis.addEventListener('click', function() {
-    if(!parenthesis){
-        $displayInput.value = $displayInput.value + '('
-    } else {
-        $displayInput.value = $displayInput.value + ')'
+  calc = $displayInput.value
+  finalResult = $result.textContent
+  callbackMemory.push( calc, finalResult)
+}
+
+//* NUMBER 
+$buttonsNumber.forEach(function ($button) {
+  $button.addEventListener("click", function () {
+    $displayInput.value = $displayInput.value + $button.textContent;
+    calculateConvertToNumber += Number($displayInput.value);
+    operation = false;
+  });
+});
+
+//* OPERATIONS
+$buttonsMathOperations.forEach(function ($button) {
+  $button.addEventListener("click", function () {
+    if(!operation){
+      getResult()
+
+      if ($result.textContent == 0) {
+        $displayInput.value = $displayInput.value + $button.textContent;
+      } else {
+        $displayInput.value = $result.textContent + $button.textContent;
+      }
+      operation = true; 
     }
+  });
+});
 
-    parenthesis = !parenthesis
+//* PARENTHESIS
+$buttonParenthesis.addEventListener("click", function () {
+  if (!parenthesis) {
+    $displayInput.value = $displayInput.value + "(";
+  } else {
+    $displayInput.value = $displayInput.value + ")";
+  }
+  parenthesis = !parenthesis;
+});
+
+//* PERCENTAGE
+$buttonPercentage.addEventListener("click", function () {
+  result = Number($result.textContent) * 100;
+  $result.textContent = result + "%";
+});
+
+//* SIGNAL +/-
+$buttonSignal.addEventListener("click", function () {
+  $result.textContent = $result.textContent * -1;
+});
+
+//* RESULT
+$resultButton.addEventListener("click", function () {
+  getResult()
+
+  $displayInput.value = "";
+  // $result.textContent = eval(result)
+});
+
+//* RESET BUTTON
+$resetButton.addEventListener("click", function () {
+  $displayInput.value = "";
+  $result.textContent = "0";
+  parenthesis = false;
+  operation = false;
+});
+
+//* CALLBACK MEMORY
+$buttonCallback.addEventListener("click", function () {
+  $memory.classList.toggle('close')
+  printMemory()
 })
 
-$buttonPercentage.addEventListener('click', function() {
-    result = Number($result.textContent) * 100
-    $result.textContent = result + '%'
+$closeCallback.addEventListener("click", function () {
+  $memory.classList.toggle('close')
 })
 
-
-$resultButton.addEventListener('click', function(){
-    result = $displayInput.value
-    if(multiply){
-        result = $displayInput.value.replace('×',"*")
-        multiply = false
-    } 
-    if (division){
-        result = $displayInput.value.replace('÷', '/')
-        division = false
-    }
-
-    console.log(result)
-    $result.textContent = eval(result).toFixed(2)
-    // $result.textContent = calculateConvertToNumber (Não funciona pq os operadores estão como string)
-})
-
-$resetButton.addEventListener('click', function() {
-    $displayInput.value =''
-    $result.textContent =''
-})
